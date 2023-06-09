f359801d61c066b8095b11d4b9ba359ccb7eccd8
justinshane13 <justinshane13@gmail.com>
2023-06-08 22:23:30 -0700

    update colors for sectorpage

--- comps/SectorPage.js
+++ comps/SectorPage.js
@@ -4,34 +4,37 @@ import Upgrade from './Upgrade';
 
 const SectorPage = ({ name, description, image, characteristics }) => {
     return (
-        <div className={styles.sectorPage}>
-            <div className={styles.sectorPageSection1}>
-                <h1>{name}</h1>
-                <div>
+        <main>
+            <div className={styles.navbarBackground}></div>
+            <div className={styles.sectorPage}>
+                <div className={styles.sectorPageSection1}>
+                    <h1>{name}</h1>
                     <div>
-                        {description.map(p => <p className={styles.sectorParagraph}>{p}</p>)}
+                        <div>
+                            {description.map(p => <p className={styles.sectorParagraph}>{p}</p>)}
+                        </div>
+                        <Image className={styles.sectorPageSection1Image} src={image} width={500} height={500} />
                     </div>
-                    <Image className={styles.sectorPageSection1Image} src={image} width={500} height={500} />
                 </div>
-            </div>
-            <div className={styles.sectorPageSection2}>
-                <p>{name}</p>
-                <h2>Key Characteristics</h2>
-                <div>
-                    {characteristics.map(c => {
-                        return (
-                            <div className={styles.sectorPageSection2Tile}>
-                                <div>
-                                    <Image className={styles.sectorPageIcon} src={c.image} width={50} height={50} />
+                <div className={styles.sectorPageSection2}>
+                    <p>{name}</p>
+                    <h2>Key Characteristics</h2>
+                    <div>
+                        {characteristics.map(c => {
+                            return (
+                                <div className={styles.sectorPageSection2Tile}>
+                                    <div>
+                                        <Image className={styles.sectorPageIcon} src={c.image} width={50} height={50} />
+                                    </div>
+                                    <p>{c.text}</p>
                                 </div>
-                                <p>{c.text}</p>
-                            </div>
-                        )
-                        })}
+                            )
+                            })}
+                    </div>
                 </div>
+                <Upgrade />
             </div>
-            <Upgrade />
-        </div>
+        </main>
     );
 }
 
